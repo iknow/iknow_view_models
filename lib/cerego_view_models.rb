6beda5c7a656707915fe9164c2a386e47597b545
Kevin Griffin <xgriffin.kevin@gmail.com>
2014-07-02 16:54:38 +0900

    Add a hook for Rails controllers to define a render method for view models

--- lib/cerego_view_models.rb
+++ lib/cerego_view_models.rb
@@ -2,4 +2,18 @@ require "cerego_view_models/version"
 require 'view_model'
 
 module CeregoViewModels
+  # expects a class that defines a "render" method
+  # accepting json as a key like ActionController::Base
+  # defines klass#render_view_model on the class
+  def self.renderable!(klass)
+    klass.class_eval do
+      def render_viewmodel(viewmodel, options = {})
+        response = Jbuilder.encode do |json|
+          ViewModel.serialize(viewmodel, json, options)
+        end
+
+        render(json: response)
+      end
+    end
+  end
 end
