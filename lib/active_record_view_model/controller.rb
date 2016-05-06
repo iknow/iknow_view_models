18eb4661d33d1b4ef7f41abb24a1742a4f7e15ea
Andrew Childs <andrew.childs@bibo.com.ph>
2016-05-06 12:40:06 +0900

    FIX Normalize user references

--- lib/active_record_view_model/controller.rb
+++ lib/active_record_view_model/controller.rb
@@ -78,6 +78,9 @@ module ActiveRecordViewModel::Controller
       raise ActiveRecordViewModel::ControllerBase::BadRequest.new('Invalid references submitted')
     end
 
+    # Normalize
+    refs ||= {}
+
     return update_hash, refs
   end
 
