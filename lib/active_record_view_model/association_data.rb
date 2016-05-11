d45e32186f6beebe362a8777d62ed177abf31d0e
Chris Andreae <chris@bibo.com.ph>
2016-05-11 17:16:46 +0900

    Make association data model lookup lazy

--- lib/active_record_view_model/association_data.rb
+++ lib/active_record_view_model/association_data.rb
@@ -3,26 +3,36 @@ class ActiveRecordViewModel::AssociationData
   delegate :polymorphic?, :collection?, :klass, :name, to: :reflection
 
   def initialize(reflection, viewmodel_classes, shared, optional)
-    if viewmodel_classes.nil?
-      # If the association isn't polymorphic, we may be able to guess from the reflection
-      model_class = reflection.klass
-      if model_class.nil?
-        raise ViewModel::DeserializationError.new("Couldn't derive target class for polymorphic association `#{reflection.name}`")
-      end
-      viewmodel_class = ActiveRecordViewModel.for_view_name(model_class.name) # TODO: improve error message to show it's looking for default name
-      viewmodel_classes = [viewmodel_class]
-    end
-
     @reflection        = reflection
-    @viewmodel_classes = Array.wrap(viewmodel_classes)
     @shared            = shared
     @optional          = optional
+    if viewmodel_classes
+      @viewmodel_classes = Array.wrap(viewmodel_classes)
+    end
+  end
 
-    @model_to_viewmodel = viewmodel_classes.each_with_object({}) do |vm, h|
+  def viewmodel_classes
+    # If we weren't given explicit viewmodel classes, try to work out from the
+    # names. This should work unless the association is polymorphic.
+    @viewmodel_classes ||=
+      begin
+        model_class = reflection.klass
+        if model_class.nil?
+          raise ViewModel::DeserializationError.new("Couldn't derive target class for polymorphic association `#{reflection.name}`")
+        end
+        viewmodel_class = ActiveRecordViewModel.for_view_name(model_class.name) # TODO: improve error message to show it's looking for default name
+        [viewmodel_class]
+      end
+  end
+
+  private def model_to_viewmodel
+    @model_to_viewmodel ||= viewmodel_classes.each_with_object({}) do |vm, h|
       h[vm.model_class] = vm
     end
+  end
 
-    @name_to_viewmodel = viewmodel_classes.each_with_object({}) do |vm, h|
+  private def name_to_viewmodel
+    @name_to_viewmodel ||= viewmodel_classes.each_with_object({}) do |vm, h|
       h[vm.view_name] = vm
     end
   end
@@ -45,7 +55,7 @@ class ActiveRecordViewModel::AssociationData
   end
 
   def viewmodel_class_for_model(model_class)
-    vm_class = @model_to_viewmodel[model_class]
+    vm_class = model_to_viewmodel[model_class]
     if vm_class.nil?
       raise ArgumentError.new("Can't find corresponding viewmodel to model '#{model_class.name}' for association '#{reflection.name}'")
     end
@@ -53,7 +63,7 @@ class ActiveRecordViewModel::AssociationData
   end
 
   def viewmodel_class_for_name(name)
-    vm_class = @name_to_viewmodel[name]
+    vm_class = name_to_viewmodel[name]
     if vm_class.nil?
       raise ArgumentError.new("Can't find corresponding viewmodel with name '#{name}' for association '#{reflection.name}'")
     end
