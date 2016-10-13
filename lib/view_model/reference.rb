a6bf6325593f49e55143e8d14295a1934df5446c
Chris Andreae <chris@bibo.com.ph>
2016-10-13 16:41:24 +0900

    Convert ViewModel::Reference to a real class

--- lib/view_model/reference.rb
+++ lib/view_model/reference.rb
@@ -1,9 +1,27 @@
 class ViewModel
   # Key to identify a viewmodel with some kind of inherent ID (e.g. an ActiveRecordViewModel)
-  Reference = Struct.new(:viewmodel_class, :model_id) do
+  class Reference
+    attr_accessor :viewmodel_class, :model_id
+
+    def initialize(viewmodel_class, model_id)
+      @viewmodel_class = viewmodel_class
+      @model_id        = model_id
+    end
+
     def to_s
       "'#{viewmodel_class.view_name}(#{model_id})'"
     end
-  end
 
+    def ==(other)
+      other.class             == self.class &&
+        other.viewmodel_class == viewmodel_class &&
+        other.model_id        == model_id
+    end
+
+    alias :eql? :==
+
+    def hash
+      [viewmodel_class, model_id].hash
+    end
+  end
 end
