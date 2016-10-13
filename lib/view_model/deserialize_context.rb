825d5fde4aacd10687d5e6c6c4eeea65195eb03c
Chris Andreae <chris@bibo.com.ph>
2016-10-13 16:41:08 +0900

    Store ancestor tree in deserialize context rather than just reference to immediate parent.

--- lib/view_model/deserialize_context.rb
+++ lib/view_model/deserialize_context.rb
@@ -1,15 +1,29 @@
 class ViewModel
   class DeserializeContext
-    attr_accessor :updated_associations, :parent_ref
+    attr_accessor :updated_associations, :parent_context, :parent_viewmodel
 
-    def initialize(parent_ref: nil)
-      self.parent_ref = parent_ref
+    def initialize(*)
     end
 
-    def for_child(parent)
-      self.dup.tap do |copy|
-        copy.parent_ref = parent.to_reference
+    def parent(idx = 0)
+      if idx == 0
+        parent_viewmodel
+      else
+        parent_context&.parent(idx - 1)
       end
     end
+
+    def parent_ref(idx = 0)
+      parent(idx)&.to_reference
+    end
+
+    def for_child(parent_viewmodel)
+      self.dup.tap do |copy|
+        copy.parent_context   = self
+        copy.parent_viewmodel = parent_viewmodel
+      end
+    end
+
+    private :parent_context, :parent_viewmodel
   end
 end
