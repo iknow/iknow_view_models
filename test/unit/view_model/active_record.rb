87d83ba0f5a6d47cf947fe41e86e8dff7378af6c
Chris Andreae <chris@bibo.com.ph>
2016-10-31 16:09:24 +0900

    Move attribute view tests into VM::ActiveRecord test suite
    To avoid a minitest-hooks bug.

--- test/unit/view_model/active_record.rb
+++ test/unit/view_model/active_record.rb
@@ -246,6 +246,61 @@ class ViewModel::ActiveRecordTest < ActiveSupport::TestCase
   end
 
 
+  # Tests for overriding the serialization of attributes using custom viewmodels
+  class CustomAttributeViewsTests < ActiveSupport::TestCase
+    include ARVMTestUtilities
+
+    class ComplexAttributeView < ViewModel
+      attribute :array
+
+      def serialize_view(json, serialize_context:)
+        json.a array[0]
+        json.b array[1]
+      end
+
+      def self.deserialize_from_view(hash_data, deserialize_context:)
+        array = [hash_data["a"], hash_data["b"]]
+        self.new(array)
+      end
+    end
+
+    def before_all
+      super
+      build_viewmodel(:Pair) do
+        define_schema do |t|
+          t.column :pair, "integer[]"
+        end
+
+        define_model do
+        end
+
+        define_viewmodel do
+          attribute :pair, using: ComplexAttributeView
+        end
+      end
+    end
+
+    def setup
+      super
+      @pair = Pair.create!(pair: [1,2])
+    end
+
+    def test_serialize_view
+      view, _refs = serialize_with_references(PairView.new(@pair))
+
+      assert_equal({ "_type" => "Pair",
+                     "id"    => @pair.id,
+                     "pair"  => { "a" => 1, "b" => 2 }},
+                   view)
+    end
+
+    def test_create
+      view = { "_type" => "Pair", "pair" => { "a" => 3, "b" => 4 } }
+      pv = PairView.deserialize_from_view(view)
+      assert_equal([3,4], pv.model.pair)
+    end
+  end
+
   # Tests for functionality common to all ARVM instances, but require some kind
   # of relationship.
   class RelationshipTests < ActiveSupport::TestCase
