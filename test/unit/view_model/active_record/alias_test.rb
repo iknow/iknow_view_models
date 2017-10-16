80899aa8feba3135004315825ae6940af964e813
Andrew Childs <andrew.childs@bibo.com.ph>
2017-10-16 20:04:58 +0900

    Fix association type lookup for alias registration

---
+++ test/unit/view_model/active_record/alias_test.rb
@@ -0,0 +1,35 @@
+require_relative "../../../helpers/arvm_test_utilities.rb"
+require_relative "../../../helpers/arvm_test_models.rb"
+require_relative "../../../helpers/viewmodel_spec_helpers.rb"
+
+require "minitest/autorun"
+
+require "view_model/active_record"
+
+class ViewModel::ActiveRecord::Alias < ActiveSupport::TestCase
+  include ARVMTestUtilities
+  extend Minitest::Spec::DSL
+
+  include ViewModelSpecHelpers::ParentAndBelongsToChild
+
+  def child_attributes
+    super.merge(
+      viewmodel: ->(v) do
+        add_view_alias "ChildA"
+        add_view_alias "ChildB"
+      end
+    )
+  end
+
+  it "permits association types to be aliased" do
+    %w(Child ChildA ChildB).each do |view_alias|
+      view = {
+        "_type" => viewmodel_class.view_name,
+        "child" => { "_type" => view_alias },
+      }
+
+      parent = viewmodel_class.deserialize_from_view(view).model
+      assert_instance_of(child_model_class, parent.child)
+    end
+  end
+end
