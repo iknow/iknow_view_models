e484757229db502811434e9fd49da10d7c3ddd73
Chris Andreae <chris@bibo.com.ph>
2018-04-19 14:07:43 +0900

    Add contexts to VM::AR::Visitor

--- lib/view_model/active_record/visitor.rb
+++ lib/view_model/active_record/visitor.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class ViewModel::ActiveRecord::Visitor
   attr_reader :visit_shared
 
@@ -5,8 +7,8 @@ class ViewModel::ActiveRecord::Visitor
     @visit_shared = visit_shared
   end
 
-  def visit(node)
-    return unless pre_visit(node)
+  def visit(node, context: nil)
+    return unless pre_visit(node, context: context)
 
     class_name = node.class.name.underscore.gsub('/', '__')
     visit      = :"visit_#{class_name}"
@@ -14,7 +16,7 @@ class ViewModel::ActiveRecord::Visitor
 
     visit_children =
       if respond_to?(visit, true)
-        self.send(visit, node)
+        self.send(visit, node, context: context)
       else
         true
       end
@@ -27,23 +29,25 @@ class ViewModel::ActiveRecord::Visitor
         next if member_data.shared? && !visit_shared
         children = Array.wrap(node._read_association(name))
         children.each do |child|
-          self.visit(child)
+          if context
+            child_context = context.for_child(node, association_name: name, root: member_data.shared?)
+          end
+          self.visit(child, context: child_context)
         end
       end
     end
 
-    self.send(end_visit, node) if respond_to?(end_visit, true)
+    self.send(end_visit, node, context: context) if respond_to?(end_visit, true)
 
-    post_visit(node)
+    post_visit(node, context: context)
   end
 
   # Invoked for all node types before visit, may cancel visit by returning
   # false.
-  def pre_visit(node)
+  def pre_visit(_node, context: nil)
     true
   end
 
   # Invoked for all node types after visit.
-  def post_visit(node)
-  end
+  def post_visit(_node, context: nil); end
 end
