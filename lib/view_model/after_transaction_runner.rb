7dd3179e52da194123ef30facecf9e5c632d1c37
Chris Andreae <chris@bibo.com.ph>
2019-09-03 17:59:06 +0900

    Expose a broader API for the AfterTransactionRunner

--- lib/view_model/after_transaction_runner.rb
+++ lib/view_model/after_transaction_runner.rb
@@ -4,15 +4,30 @@
 # `add_to_transaction`, the abstract method `after_transaction` will be invoked
 # by AR's callbacks.
 module ViewModel::AfterTransactionRunner
-  def committed!; end
+  # Rails' internal API
+  def committed!(*)
+    after_commit
+  end
 
   def before_committed!
-    after_transaction
+    before_commit
   end
 
   def rolledback!(*)
-    after_transaction
+    after_rollback
   end
+
+  def trigger_transactional_callbacks?
+    true
+  end
+
+  # Our simplified API
+
+  def before_commit; end
+
+  def after_commit; end
+
+  def after_rollback; end
 
   def add_to_transaction
     if connection.transaction_open?
