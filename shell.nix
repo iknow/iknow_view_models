e71bfa6c1052385883ef2a05d0a2dad396e1eb33
Chris Andreae <chris@bibo.com.ph>
2019-12-16 19:34:38 +0900

    RubyGems patch is no longer required

--- shell.nix
+++ shell.nix
@@ -6,15 +6,5 @@ with import <nixpkgs> {};
 
   gemConfig = (defaultGemConfig.override { postgresql = postgresql_11; });
 
-  ruby = ruby_2_6.overrideAttrs (attrs: {
-    patches = (attrs.patches or []) ++ [
-      # RubyGems has a regression where you can no longer build certain gems
-      # outside their directory. Until this is merged, patch from the pull
-      # request.
-      (fetchpatch {
-        url = https://patch-diff.githubusercontent.com/raw/rubygems/rubygems/pull/2596.patch;
-        sha256 = "0m1s5brd30bqcr8v99sczihm83g270philx83kkw5bpix462fdm3";
-      })
-    ];
-  });
+  ruby = ruby_2_6;
 }).env
