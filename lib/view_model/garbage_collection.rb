ec803e8717fc9ebeee0bf049efba97ae93ea95ab
Chris Andreae <chris@bibo.com.ph>
2021-01-27 12:14:46 +0900

    GC worklist: use set instead of array, avoid requeueing visited refs

--- lib/view_model/garbage_collection.rb
+++ lib/view_model/garbage_collection.rb
@@ -8,35 +8,36 @@ class ViewModel::GarbageCollection
       roots      = serialization.except('references')
       references = serialization['references']
 
-      queue = []
-      seen = Set.new
+      worklist = Set.new(collect_references(roots))
+      visited  = Set.new
 
-      collect_references(roots, queue)
-
-      while (live = queue.shift)
-        next if seen.include?(live)
-        seen << live
-        collect_references(references[live], queue)
+      while (live = worklist.first)
+        worklist.delete(live)
+        visited << live
+        collect_references(references[live]) do |ref|
+          worklist << ref unless visited.include?(ref)
+        end
       end
 
-      references.keep_if { |ref, _val| seen.include?(ref) }
+      references.keep_if { |ref, _val| visited.include?(ref) }
     end
 
     private
 
-    def collect_references(tree, acc = Set.new)
+    ## yield each reference encountered in tree
+    def collect_references(tree, &block)
+      return enum_for(__method__, tree) unless block_given?
+
       case tree
       when Hash
         if tree.size == 1 && (ref = tree[ViewModel::REFERENCE_ATTRIBUTE])
-          acc << ref
+          block.(ref)
         else
-          tree.each_value { |t| collect_references(t, acc) }
+          tree.each_value { |t| collect_references(t, &block) }
         end
       when Array
-        tree.each { |t| collect_references(t, acc) }
+        tree.each { |t| collect_references(t, &block) }
       end
-
-      acc
     end
   end
 end
